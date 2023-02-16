a0fe5eeacc3779324c221bad27806741b3e930f4
Shital Padwal <shitalp1@bitwiseglobal.net>
2023-02-16 15:03:11 +0530

    version3

--- src/pract_1.java
+++ src/pract_1.java
@@ -3,6 +3,6 @@ import javax.sql.rowset.spi.SyncResolver;
 public class pract_1 
 {
     public static void main(String[] args) {
-        System.out.println("version 2");
+        System.out.println("version 3");
     }
 }
